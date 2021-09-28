d5f6aacae21e99160180a7951259fa886abe1dec
akpekig <57150149+akpekig@users.noreply.github.com>
2021-09-28 02:17:38 +0100

    Move map and attribution elements

--- src/App.js
+++ src/App.js
@@ -23,14 +23,7 @@ export default function App() {
           IP Address Location Timezone UTC ISP
         </form>
       </header>
-      <div id="map"></div>
-      <div className="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
+      <Datalist addressIP={addressIP} location={location} serviceProvider={serviceProvider} />
     </div>
   );
 }
@@ -41,25 +34,11 @@ const loader = new Loader({
   apiKey: process.env.GOOGLE_MAPS_API_KEY,
   version: "weekly",
 });
-
-loader.load().then(() => {
-  // eslint-disable-next-line no-undef
-  let map = new google.maps.Map(document.getElementById("map"), {
+const Datalist = (props) => {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
 });
-
-var http = require('http');
-
-var ip = '8.8.8.8';
-var api_key = process.env.GEO_IPIFY_API_KEY;
-var api_url = 'https://geo.ipify.org/api/v1?';
-
-var url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip;
-
-http.get(url, function(response) {
-    var str = '';
-    response.on('data', function(chunk) { str += chunk; });
-    response.on('end', function() { console.log(str); });
-}).end();
+  return (
+  );
+}
