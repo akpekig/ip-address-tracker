60e7385cff2a87fbc2f88ff46b34bc85e4ea4e78
akpekig <57150149+akpekig@users.noreply.github.com>
2021-09-28 02:18:57 +0100

    Add IP Tracker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import { BsChevronRight } from "react-icons/bs";
 import { Loader } from "@googlemaps/js-api-loader";
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 
+// Initialise API for IP tracker
+var api_key = process.env.REACT_APP_GEO_IPIFY_KEY;
+var api_url = 'https://geo.ipify.org/api/v1?apiKey=' + api_key + '&ipAddress=';
+/*e.preventDefault();
+            let url = api_url + addressIP;
+            require('http').get(url, function(response) {
+              var str = '';
+              response.on('data', function(chunk) { str += chunk; });
+              response.on('end', function() { console.log(str); });
+          }).end();*/
 export default function App() {
+  const [input, setInput] = useState ("");
+  const [addressIP, setAddressIP] = useState("8.8.8.8");
+  const [location, setLocation] = useState({ "city": "Mountain View",
+                                             "region": "California",
+                                             "country": "US",
+                                             "postcode": "94035",
+                                             "timezone": "-07:00",
+                                             "lat": "37.38605",
+                                             "lng": "-122.08385"});
+  const [serviceProvider, setServiceProvider] = useState("Google LLC");                               
+  const handleInput = (event) => {
+    let localInput = input.toString();
+    let arrayIP = localInput.split("."); // Split entered IP address up for further validation
+      for ( let i = 0 ; i < arrayIP.length ; i++ ) {
+        // Invalidate private IP addresses
+        if( parseInt(arrayIP[0]) < 192 &&
+              parseInt(arrayIP[i]) <= 255 && 
+              parseInt(arrayIP[0]) !== 10 &&
+              parseInt(arrayIP[0]) !== 0 &&
+              (parseInt(arrayIP[0]) !== 172 && 
+                (parseInt(arrayIP[1]) < 16 || parseInt(arrayIP[1]) > 31)) ) {
+                  // Allow input as IP address
+                  setAddressIP(localInput);
+        } else { // Return validity error
+          console.log(localInput);
+        }
+      }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleInput(event);
+    let url = api_url + addressIP;
+    // Run IP tracker
+    require('http').get(url, function(response) {
+      var str = '';
+      response.on('data', function(chunk) { str += chunk; });
+      response.on('end', function() {
+        // Convert response into JS Object
+        let responseJSON = JSON.parse(str);
+        console.log(responseJSON); 
+        setLocation(responseJSON.location);
+        setServiceProvider(responseJSON.isp);
+      });
+    }).end();
+  }
+
   return (
     <div className="App">
       <header>
         <h1>IP Address Tracker</h1>
-        <form>
+        <form 
+            onSubmit={e => handleSubmit(e)}>
           <input
             type="text"
             id="ip-address"
             name="addressIP"
+            title="Enter a valid IP address"
+            minLength="7"
+            maxLength="15"
+            pattern="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
             placeholder="Search for any IP address or domain"
-            minlength="7"
-            maxlength="15"
-            size="20"
+            onChange={e => setInput(e.target.value)}
+            onInvalid={e => e.target.setCustomValidity("Please enter a valid IP address!")}
             required
           />
-          <button type="submit"><BsChevronRight /></button>
-          IP Address Location Timezone UTC ISP
+          <button type="submit"><BsChevronRight /></button> 
         </form>
       </header>
       <Datalist addressIP={addressIP} location={location} serviceProvider={serviceProvider} />
@@ -28,17 +88,44 @@ export default function App() {
   );
 }
 
-require('dotenv').config();
-
+// Initialise API for GMaps
 const loader = new Loader({
-  apiKey: process.env.GOOGLE_MAPS_API_KEY,
+  apiKey: process.env.REACT_APP_GMAPS_KEY,
   version: "weekly",
 });
 const Datalist = (props) => {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  let coordinates = {lat: parseFloat(props.location.lat), lng: parseFloat(props.location.lng)};
+  loader.load().then(() => {
+    // eslint-disable-next-line no-undef
+    let map = new google.maps.Map(document.getElementById("map"), {
+      center: coordinates,
+      zoom: 8,
+    });
+    // eslint-disable-next-line no-undef
+    new google.maps.Marker({
+      position: coordinates,
+      map,
+      title: "IP Tracker",
+    });
   });
-});
   return (
+    <dl>
+          <div>
+            <dt>IP Address</dt>
+            <dd>{props.addressIP}</dd>
+          </div>
+          <div>
+            <dt>Location</dt>
+            <dd>{props.location.city}, {props.location.region}, {props.location.country}</dd>
+          </div>
+          <div>
+            <dt>Timezone</dt>
+            <dd>UTC {props.location.timezone}</dd>
+          </div>
+          <div>
+            <dt>ISP</dt>
+            <dd>{props.serviceProvider}</dd>
+          </div>
+        </dl>
   );
 }
